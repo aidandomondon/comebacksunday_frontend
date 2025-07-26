e05fa02e134e3ca74c21b3d4a2372486c65e7c98
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-07-25 20:30:48 -0400

    Adds login and registration pages

---
+++ src/pages/register.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styles from './login.module.css'
+import Link from 'next/link';
+
+export default function Register() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    // process changes to the value of the username input field
+    function handleUsernameChange(event) {
+        setUsername(event.target.value);
+    }
+
+    // process changes to the value of the password input field
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
+    return (
+        <div className={styles.base}>
+            <h1 className={styles.pageTitle}>Welcome</h1>
+            <form className={styles.form}>
+                <div className={styles.formSection}>
+                    <label>Username</label>
+                    <input 
+                        name="username" 
+                        type="text" 
+                        value={username}
+                        onChange={handleUsernameChange}
+                        className={styles.formInput}
+                    ></input>
+                </div>
+                <div className={styles.formSection}>
+                    <label>Password</label>
+                    <input 
+                        name="password" 
+                        type="password" 
+                        value={password}
+                        onChange={handlePasswordChange}
+                        className={styles.formInput}
+                    ></input>
+                </div>
+                <button
+                    type="submit"
+                    className={[styles.formInput, styles.formButton].join(' ')}
+                >
+                    <p className={styles.formButtonText}>Log In</p>
+                </button>
+            </form>
+            <div className={styles.redirectToRegistration}>
+                <p>Already have an account? <Link href="/login">Log in.</Link></p>
+            </div>
+        </div>
+    );
+}
